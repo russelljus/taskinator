3b6a258b83d9c91ac0b0a24fe60a3f26f2a1f5c2
Justin Russell <justinrussell@Justins-MacBook-Pro.local>
2021-04-03 19:16:06 -0600

    js createTaskHandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,17 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) { 
   event.preventDefault(); 
+  var taskNameInput = document.querySelector("input[name='task-name']");
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  console.log(taskTypeInput);
+
+  
+
 
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
+  listItemEl.textContent = taskNameInput; 
   tasksToDoEl.appendChild(listItemEl); 
   }; 
 
